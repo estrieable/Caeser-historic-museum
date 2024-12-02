2a6362b49a3df6dd8d2bf30cbef6ab44e6b8ea56
NausAlexus <naus.lexus@mail.ru>
2024-12-02 18:03:26 +0300

    Создание блока explore в main

--- js/script.js
+++ js/script.js
@@ -1,36 +1,17 @@
-const $ = jQuery;
+// Отображение данных Explore_____________________________
+async function loadExploreData() {
+    try {
+        const response = await fetch('../src/config/main-config.json');
+        const data = await response.json();
 
-function loadText(){
-    // const request = new XMLHttpRequest();
-    // request.open("GET", '/src/load-this.txt');
-    // request.onload = function(){
-    //     let text = this.response;
-    //     document.getElementById("text-wrapper").innerHTML = text;
-    // };
+        const exploreTextElement = document.querySelector('.explore-text');
+        const exploreTitleElement = document.querySelector('.explore-title');
 
-    // request.send();
-    fetch('/src/load-this.txt')
-    .then(res => res.text())
-    .then(text => text.html(text))
-};
-const LoadText = $("#load-text");
-LoadText.on("click", loadText);
-
-function loadUsers() {
-    const request = new XMLHttpRequest();
-    request.open("GET", '/src/my-users.json');
-    request.onload = function() {
-        let users = this.response;
-        const userData = JSON.parse(users);
-
-        $("#users-wrapper").html("")
-        userData.map(el => {
-            $("#users-wrapper").append(`<li id="user-${el.id}" class="${el.role}">${el.name}</li>`)
-        })
+        exploreTextElement.textContent = data.explore[0]['explore-text'];
+        exploreTitleElement.textContent = data.explore[0]['explore-title'];
+    } catch (error) {
+        console.error('Error loading data:', error);
     }
-    request.send();
 }
 
-const LoadUser = $("#load-users");
-LoadUser.on("click", loadUsers);
-
+loadExploreData();
