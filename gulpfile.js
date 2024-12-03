d4f09160d7fe27e31e7d8df269626ebdb6280e0f
NausAlexus <naus.lexus@mail.ru>
2024-12-03 20:38:17 +0300

    Собрал проект

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const cssnano = require('gulp-cssnano');
+const sourcemaps = require('gulp-sourcemaps');
+
+// Путь к CSS файлам
+const paths = {
+    styles: {
+        src: 'css/_blocks/*.css', // Путь к вашим блокам CSS
+        dest: 'dist/css/'          // Куда сохранить итоговый файл
+    }
+};
+
+// Задача для обработки стилей
+function styles() {
+    return gulp.src(paths.styles.src)
+        .pipe(sourcemaps.init()) // Инициализация сопоставления
+        .pipe(concat('style.css')) // Объединение в style.css
+        .pipe(cssnano()) // Минификация
+        .pipe(sourcemaps.write('.')) // Запись карт исходников
+        .pipe(gulp.dest(paths.styles.dest)); // Сохранение в указанную директорию
+}
+
+// Задача по умолчанию
+gulp.task('default', styles);
